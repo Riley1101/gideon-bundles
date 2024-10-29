11a1873448a744a2d8e265bf9ae13d4cc8996a81
Arkar Myat <arkardev00@gmail.com>
2024-10-28 20:59:10 -0400

    add item to cache

--- scalable/src/cache.js
+++ scalable/src/cache.js
@@ -2,6 +2,23 @@ import { Level } from "level";
 import path from "path";
 
 /**
+ * @typedef {Level<string, string>} Cache
+ */
+
+/**
  * @description Create a cache  for bundler
  */
-export const Cache = new Level(path.join(process.cwd()), ".bundler-cache");
+export const Cache = new Level(path.join(process.cwd()), {
+  valueEncoding: "string",
+});
+
+/**
+ * @param {Cache} cache Cache block
+ * @param {string} k Cache key
+ * @param {string} v Cache value
+ * @returns {string}
+ */
+export function addAssetToCache(cache, k, v) {
+  cache.put(k, v);
+  return k;
+}
