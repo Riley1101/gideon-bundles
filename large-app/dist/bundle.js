073d1b256ab778e2d71c1df8cf5eb734ad70e539
Arkar Myat <arkardev00@gmail.com>
2024-11-19 22:13:26 -0500

    build a bundler !

--- large-app/dist/bundle.js
+++ large-app/dist/bundle.js
@@ -1,29 +1,30 @@
+(function (modules) {
+  function require(id) {
+    if (!id) {
+      return;
+    }
+    const [fn, mapping] = modules[id];
 
-      (function(modules) {
-        function require(id) {
-          if(!id){
-            return ;
-          }
-          const [fn, mapping] = modules[id];
+    function localRequire(name) {
+      return require(mapping[name]);
+    }
 
-          function localRequire(name) {
-            return require(mapping[name]);
-          }
+    const module = { exports: {} };
 
-          const module = { exports : {} };
+    fn(localRequire, module, module.exports);
 
-          fn(localRequire, module, module.exports);
+    return module.exports;
+  }
 
-          return module.exports;
-        }
+  require("/js/index.js");
+})({
+  "/js/index.js": [
+    function (require, module, exports) {
+      "use strict";
 
-        require("/js/index.js");
-      })({"/js/index.js": [
-          function (require, module, exports) {
-            "use strict";
-
-const h1 = document.querySelector("#title");
-h1.innerText = "Hello World!";
-          },
-          {},
-        ],})
+      const h1 = document.querySelector("#title");
+      h1.innerText = "Hello World!";
+    },
+    {},
+  ],
+});
--- scalable/dist/bundle.js
+++ scalable/dist/bundle.js
@@ -1,69 +1,79 @@
+(function (modules) {
+  function require(id) {
+    const [fn, mapping] = modules[id];
 
-      (function(modules) {
-        function require(id) {
-          const [fn, mapping] = modules[id];
+    function localRequire(name) {
+      return require(mapping[name]);
+    }
 
-          function localRequire(name) {
-            return require(mapping[name]);
-          }
+    const module = { exports: {} };
 
-          const module = { exports : {} };
+    fn(localRequire, module, module.exports);
 
-          fn(localRequire, module, module.exports);
+    return module.exports;
+  }
 
-          return module.exports;
-        }
+  require("/test/index.js");
+})({
+  "/test/index.js": [
+    function (require, module, exports) {
+      "use strict";
 
-        require("/test/index.js");
-      })({"/test/index.js": [
-          function (require, module, exports) {
-            "use strict";
+      var _apple = require("./apple");
+      var _apple2 = _interopRequireDefault(_apple);
+      var _orange = require("./orange");
+      var _nested = require("./nested");
+      var _nested2 = _interopRequireDefault(_nested);
+      function _interopRequireDefault(e) {
+        return e && e.__esModule ? e : { default: e };
+      }
+      const a = 12;
+      const b = 21;
+      console.log(a + b);
+      console.log(_apple2.default);
+      console.log(_nested2.default);
+      (0, _orange.test)();
+    },
+    {
+      "./orange": "/test/orange.js",
+      "./nested": "/test/nested/index.js",
+      "./apple": "/test/apple.js",
+    },
+  ],
+  "/test/orange.js": [
+    function (require, module, exports) {
+      "use strict";
 
-var _apple = require("./apple");
-var _apple2 = _interopRequireDefault(_apple);
-var _orange = require("./orange");
-var _nested = require("./nested");
-var _nested2 = _interopRequireDefault(_nested);
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-const a = 12;
-const b = 21;
-console.log(a + b);
-console.log(_apple2.default);
-console.log(_nested2.default);
-(0, _orange.test)();
-          },
-          {"./orange":"/test/orange.js","./nested":"/test/nested/index.js","./apple":"/test/apple.js"},
-        ],"/test/orange.js": [
-          function (require, module, exports) {
-            "use strict";
+      Object.defineProperty(exports, "__esModule", {
+        value: true,
+      });
+      exports.test = test;
+      function test() {
+        console.log("hello from test");
+      }
+    },
+    {},
+  ],
+  "/test/nested/index.js": [
+    function (require, module, exports) {
+      "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+      Object.defineProperty(exports, "__esModule", {
+        value: true,
+      });
+      exports.default = "Nested Module";
+    },
+    {},
+  ],
+  "/test/apple.js": [
+    function (require, module, exports) {
+      "use strict";
+
+      Object.defineProperty(exports, "__esModule", {
+        value: true,
+      });
+      exports.default = "APPLE WORLD";
+    },
+    {},
+  ],
 });
-exports.test = test;
-function test() {
-  console.log("hello from test");
-}
-          },
-          {},
-        ],"/test/nested/index.js": [
-          function (require, module, exports) {
-            "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = "Nested Module";
-          },
-          {},
-        ],"/test/apple.js": [
-          function (require, module, exports) {
-            "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = "APPLE WORLD";
-          },
-          {},
-        ],})
